14099f5d1f721962cea2aa548ce84164c06d5236
Félix JULLIEN <jullienfelix@gmail.com>
2023-08-16 16:27:47 +0200

    Microphone: Setup openai speech-to-text

---
+++ module_microphone/README.md
@@ -0,0 +1,37 @@
+# Module microphone
+
+## Setup
+
+### Dependencies
+
+```sh
+# For pyaudio
+$ sudo apt-get install portaudio19-dev
+
+```
+
+### OpenAi API configuration
+
+- Create an account on https://platform.openai.com/account/
+- Generate an API key, copy the API key
+- in a terminal 
+
+``` sh
+$ export OPENAI_API_KEY=replace_with_your_api_key
+$ export OPENAI_ORG_ID=replace_with_your_organisation_id
+
+# Find your organisation ID at https://platform.openai.com/account/org-settings
+```
+
+
+## Info
+
+These dependencies are in `../requirement.txt`
+```
+SpeechRecognition  3.10.0
+PyAudio            0.2.13
+pydub              0.25.1
+```
+
+
+
--- module_microphone/module.py
+++ module_microphone/module.py
@@ -1,3 +1,30 @@
+import os
+import openai
+
+
+def openai_setup():
+    """
+    Setup OpenAI authentification, expect environment variables:
+      - OPENAI_ORG_ID to contain your OpenAI organisation ID
+      - OPENAI_API_KEY to contain your API Key.
+    """
+    openai.organization = os.getenv("OPENAI_ORG_ID")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.Model.list()
+
+
+def openai_speech_to_text(audio_file_path: str) -> str:
+    """
+    Transcribe the audio file at path `audio_file_path` into text using OpenAI
+    """
+    with open(audio_file_path, "rb") as audio_file:
+        response = openai.Audio.transcribe("whisper-1", audio_file)
+    transcribed_text = response["text"]
+    return transcribed_text
+
 
 def run():
     print("Hello Module Microphone")
+    openai_setup()
+    txt = openai_speech_to_text("./module_microphone/bonjour42.mp3")
+    print(txt)
