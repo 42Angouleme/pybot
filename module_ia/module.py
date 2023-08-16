06faeec6ab1e7c156b071158c6868981cc6ac3bf
Zied Harzi <zharzi@student.42angouleme.fr>
2023-08-16 17:29:25 +0200

    feat functions of module

--- module_ia/module.py
+++ module_ia/module.py
@@ -1,3 +1,44 @@
 
+def read_input():
+    print("")
+
+def parse():
+    print("")
+
+def check_API():
+    print("")
+
+def check_interaction():
+    print("")
+
+def transmit_to_API():
+    print("")
+
+def receive_from_API():
+    print("")
+
+def check_before_delivery():
+    print("")
+
+def deliver_answer():
+    print("")
+
 def run():
-    print("Hello Module (open)IA")
+    # print("Hello Module (open)IA")
+    read_input()
+    parse()
+    check_API()
+    check_interaction()
+    transmit_to_API()
+    receive_from_API()
+    check_before_delivery()
+    deliver_answer()
+
+# input
+# parsing
+# check la connexion à l'API (la faire)
+# check du sujet de la question
+# transmission
+# reception
+# check la reponse
+# délivre un fichier
