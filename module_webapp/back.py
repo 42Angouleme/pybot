d1e68e31342d1da602c652576207150c5c48cb31
Pixailz <pixailz1@gmail.com>
2023-08-22 23:26:59 +0200

    [webapp] clean up undefined variables

--- module_webapp/back.py
+++ module_webapp/back.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.10
 
-from .server import Server, ServerType
+from .server import Server, ST
+from .config import WEB_BACK_LSTN_HOST
 
 class BackServer(Server):
-	def __init__(self, port = 3000):
-		super(FrontServer, self).__init__(port, ServerType.BACK)
+	def __init__(self, port = 3000, listen_host = WEB_BACK_LSTN_HOST):
+		self.server = Server(port, ST.FRONT, listen_host)
 
 if __name__ == "__main__":
 	pass
