63ac8fba9e96889814f6352fc9475dc0fe8aa262
Félix JULLIEN <jullienfelix@gmail.com>
2023-08-22 10:44:50 +0200

    fix(main): Fix main module chat bot
    - update for module_microphone
    - comment module_camera import -> missing dependency
    - cleanup

--- main.py
+++ main.py
@@ -1,52 +1,38 @@
 #! /bin/python3
 import module_microphone as microphone
 import module_audio as audio
-import module_camera as camera
+
+# TODO an import is missing for skimage
+# import module_camera as camera
 import module_ia as ia
 import module_ecran as ecran
-from module_microphone.speech_to_text import (
-    speech_to_text,
-    openai_setup,
-    openai_speech_to_text,
-)
-from module_microphone import Microphone
+
+from module_microphone import listen
 import asyncio
 
 
-# def main():
-#    # microphone.run()
-#    txt = speech_to_text(10)
-#    if txt:
-#        audio.run(txt)
-# camera.run()
-# ia.run()
-# ecran.run()
-
-
-async def user_flow():
-    openai_setup()
-    # txt = speech_to_text()
-
+async def chat_bot():
+    print('J\'écoute... à tout moment tu peux dire le mot "STOP" pour arrêter.')
     while True:
-        input("Press anything to continue")
-        micro = Microphone()
-        # Run the recording for 4 sec maximum, without blocking
-        micro.start_record(10)
-        input("Press enter to stop recording...")
-        filepath = micro.stop()
-        if not filepath:
-            return
-        question = openai_speech_to_text(filepath)
-        if not question:
-            return
-        answer = ia.run(question)
-        if not answer:
-            return
-        audio.run(answer)
+        try:
+            question = listen.one_phrase().as_text()
+            print("Tu as dis: " + question)
+            if "stop" in question.lower():
+                audio.run(txt="Tu as dis STOP... au revoir, humain.")
+                break
+            answer = ia.run(question)
+            if answer:
+                audio.run(txt=answer)
+        except Exception as e:
+            print(f"Une erreur est survenue: {e}")
+            break
+        print('J\'écoute... à tout moment tu peux dire le mot "STOP" pour arrêter.')
+
+    print("Bye !")
 
 
 def main():
-    asyncio.run(user_flow())
+    asyncio.run(chat_bot())
     # microphone.run()
     # camera.run()
     # ia.run()
