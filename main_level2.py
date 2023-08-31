a84ad5aa573a69da40c14f2a25926fc972eb082b
mdaadoun <daadounmichael@gmail.com>
2023-08-31 16:31:20 +0200

    clean

--- main_level2.py
+++ main_level2.py
@@ -1,15 +1,12 @@
 #! venv/bin/python3
 
 import module_audio as audio
-from robot import Robot
-robot = Robot()
-
-# import module_microphone as microphone
+import module_microphone as micro
 
 # TODO an import is missing for skimage
 # import module_ia as ia
 
-# from module_microphone import listen
+# from module_micro import listen
 
 
 async def chat_bot():
@@ -33,17 +30,14 @@ async def chat_bot():
 
 def main():
     # asyncio.run(chat_bot())
-    # microphone.run()
+    micro.run()
     # audio.run()
     # ia.run()
     # ecran.run()
-    robot.configurer()
-    robot.allumer_ecran()
-    robot.allumer_camera()
 
 
 if __name__ == "__main__":
-    print("=" * 42)
-    print("HACKATHON : Robot Python - 16/17 Aout 2023")
-    print("=" * 42)
+    print("=" * 12)
+    print("Robot Python")
+    print("=" * 12)
     main()
--- robot.py
+++ robot.py
@@ -1,18 +1,22 @@
-
+from script import config
 import module_ecran as ecran
 import module_camera as camera
 
 
 class Robot:
     def __init__(self):
+        self.config = config
         self.ecran = ecran
         self.camera = camera
 
     def allumer_ecran(self):
+        """
+        Allume l'ecran pygame
+        """
         self.ecran.run()
 
     def configurer(self):
-        self.ecran.option()
+        self.config.run()
 
     def allumer_camera(self):
         self.camera.run()
