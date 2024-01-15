1e334a853661bc650400749c98a0b90beda2c197
Thibo_M <thibaud.marin@proton.me>
2024-01-15 11:07:56 +0100

    Refacto: main part5 updated
    Re-organize functions and variables
    Deleted useless lines and add comments

--- exemple_chapter_5.py
+++ exemple_chapter_5.py
@@ -3,36 +3,24 @@ from pybot import Robot
 robot = Robot()
 robot.demarrer_webapp()
 
-long = 1200
-haut = 500
-
-blanc = (255, 255, 255)
+# Initialisation des couleurs
 noir = (0, 0, 0)
-rouge = (235, 64, 52)
 rouge_sombre = (117, 23, 16)
-bleu = (52, 164, 235)
-bleu_sombre = (30, 93, 133)
-vert = (105, 230, 83)
-vert_sombre = (59, 135, 46)
-jaune = (237, 212, 66)
 jaune_sombre = (171, 128, 19)
 
-bouton_suppression = None
-bouton_deconnexion = None
-
+# Preparation des variables
 session_ouverte = False
-
 mettre_a_jour_affichage = True
-def preparer_programme():
-    global bouton_suppression, bouton_deconnexion
-    robot.allumer_ecran(long, haut)
-    robot.couleur_fond(noir)
 
-    robot.ajouter_evenement("echap", "stop")
-    bouton_deconnexion = robot.creer_bouton(200, 60, 25, 200, jaune_sombre)
-    bouton_deconnexion.ajouter_texte("Deconnexion", 5, 20)
-    bouton_suppression = robot.creer_bouton(200, 60, 25, 300, rouge_sombre)
-    bouton_suppression.ajouter_texte("Supprimer utilisateur", 5, 20)
+robot.allumer_ecran(1200, 500)
+robot.couleur_fond(noir)
+
+# Ajout des boutons
+robot.ajouter_evenement("echap", "stop")
+bouton_deconnexion = robot.creer_bouton(200, 60, 25, 200, jaune_sombre)
+bouton_deconnexion.ajouter_texte("Deconnexion", 5, 20)
+bouton_suppression = robot.creer_bouton(200, 60, 25, 300, rouge_sombre)
+bouton_suppression.ajouter_texte("Supprimer utilisateur", 5, 20)
 
 def affichage_ecran():
     global mettre_a_jour_affichage, session_ouverte
@@ -47,10 +35,12 @@ def affichage_ecran():
 
 def verifier_boutons():
     global mettre_a_jour_affichage, afficher_camera, afficher_photo, session_ouverte
+    # Mise à jour du status de la session
     if robot.verifier_session() != session_ouverte:
         session_ouverte = robot.verifier_session()
         mettre_a_jour_affichage = True
     elif session_ouverte:
+        # Vérification des boutons de session
         if bouton_suppression.verifier_contact():
             robot.supprimer_utilisateur()
             session_ouverte = False
@@ -68,10 +58,12 @@ def boucle_programme():
             robot.eteindre_ecran()
         robot.afficher_camera(300, 10)
         if not session_ouverte:
+            # Essai de connexion
             robot.connecter()
             if robot.verifier_session():
                 print(f"Hello \033[96;1m{robot.utilisateur_connecte.first_name} {robot.utilisateur_connecte.last_name}\033[0;0m !")
             else:
+                # Affichage de carte détéctée non connectée
                 carte_detectee = robot.detecter_carte()
                 if carte_detectee:
                     robot.afficher_carte_detectee(carte_detectee, 980, 200)
@@ -81,5 +73,4 @@ def boucle_programme():
 
 
 if __name__ == "__main__":
-    preparer_programme()
     boucle_programme()
