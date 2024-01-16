bce128f143bded22f60c4a01a422a64e8b866821
doc <doc@42angouleme.fr>
2024-01-16 11:08:37 +0100

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo dpkg -i download\?build=stable\&os=linux-deb-x64
+sudo curl -O code.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+sudo dpkg -i code.deb
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install -y python3.10 python3.10-venv python3-pip
