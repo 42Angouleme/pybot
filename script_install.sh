41ab88b0f1829d127b9262e06f6dd600a4fa37f6
doc <doc@42angouleme.fr>
2024-01-16 10:53:27 +0100

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flatpak install flathub com.visualstudio.code
+flatpak install --user -y flathub com.visualstudio.code
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install -y apt-transport-https python3.10 python3.10-venv python3-pip
