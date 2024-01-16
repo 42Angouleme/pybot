4741c53217f2d907877f88d56cf6de152e2beb41
doc <doc@42angouleme.fr>
2024-01-16 10:36:54 +0100

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-apt update && apt upgrade python3
-apt install -y python3.10-venv python3-pip code
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install -y python3.10 python3.10-venv python3-pip code
 
 code --install-extension ms-python.python
 code --install-extension MS-CEINTL.vscode-language-pack-fr
