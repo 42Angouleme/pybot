d22e75a6dbc0266b882f0f8e575377bfa1f828dd
doc <doc@42angouleme.fr>
2024-01-16 11:19:34 +0100

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg > /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update && install -y code
