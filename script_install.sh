dba54cf220f739506f95cb5cef332e653d6e9552
doc <doc@42angouleme.fr>
2024-01-16 09:42:30 +0100

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install python3.10-venv python3-pip code
+apt install --y python3.10-venv python3-pip code
 
 code --install-extension ms-python.python
 code --install-extension MS-CEINTL.vscode-language-pack-fr
