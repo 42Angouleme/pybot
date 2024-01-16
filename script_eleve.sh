15d37ab37e32543dc7a069ec5798a0254f82bd42
doc <doc@42angouleme.fr>
2024-01-16 09:57:58 +0100

    Update script_eleve.sh

--- script_eleve.sh
+++ script_eleve.sh
@@ -4,4 +4,4 @@ rm -rf .venv
 /usr/bin/python3.10 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-echo "export `cat .env_to_rename` PATH=$HOME/pybot/.venv/bin:$PATH" > $HOME/.bashrc
+echo "export `cat .env_to_rename | tr '\n' ' '` PATH=$HOME/pybot/.venv/bin:$PATH" > $HOME/.bashrc
