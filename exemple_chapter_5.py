355eee9a46ee3a96c4159919873e936f3ecc82b0
Thibo_M <thibaud.marin@proton.me>
2024-01-22 05:19:40 +0100

    Refacto: Modular main for better lib experience
    - Exemples 5 and 6 are now using the modular way:
    	- preparation/ loop dissociation
    	- Called in the main loop not modified by students

--- exemple_chapter_5.py
+++ exemple_chapter_5.py
@@ -1,32 +1,23 @@
-from pybot import Robot
+from pybot import Robot, Couleur
 
+
+# --- GENERAL ---
 robot = Robot()
 robot.demarrer_webapp()
-
-# Initialisation des couleurs
-noir = (0, 0, 0)
-rouge_sombre = (117, 23, 16)
-jaune_sombre = (171, 128, 19)
-
-# Preparation des variables
-session_ouverte = False
 mettre_a_jour_affichage = True
-derniere_carte_detectee = None
+largeur_fenetre = 1200
+hauteur_fenetre = 500
 
-robot.creer_fenetre(1200, 500)
-robot.couleur_fond(noir)
 
-# Ajout des boutons
-robot.ajouter_evenement("echap", "stop")
-bouton_deconnexion = robot.creer_bouton(200, 60, 25, 200, jaune_sombre)
-bouton_deconnexion.ajouter_texte("Deconnexion", 5, 20)
-bouton_suppression = robot.creer_bouton(200, 60, 25, 300, rouge_sombre)
-bouton_suppression.ajouter_texte("Supprimer utilisateur", 5, 20)
-bouton_creation = robot.creer_bouton(200, 60, 980, 100, (0, 150, 0))
-bouton_creation.ajouter_texte("Créer  utilisateur", 20, 20)
+# --- FENETRE ---
+# - Preparation -
+robot.creer_fenetre(largeur_fenetre, hauteur_fenetre)
+robot.couleur_fond(Couleur.NOIR)
 
-def affichage_ecran():
+# - Boucle -
+def boucle_fenetre():
     global mettre_a_jour_affichage, session_ouverte
+    robot.afficher_camera(300, 10)
     if mettre_a_jour_affichage:
         robot.afficher_fond()
         if session_ouverte:
@@ -38,7 +29,50 @@ def affichage_ecran():
         robot.afficher_texte("CREATION", 1000, 5, 30, (255, 255, 255))
         mettre_a_jour_affichage = False
 
-def verifier_boutons():
+
+# --- EVENEMENTS ---
+# - Preparation -
+robot.ajouter_evenement("echap", "stop")
+
+# - Boucle -
+def boucle_evenements():
+    events = robot.verifier_evenements()
+    if "stop" in events:
+        robot.fermer_fenetre()
+
+
+# --- SESSION ---
+# - Preparation -
+session_ouverte = False
+derniere_carte_detectee = None
+
+# - Boucle -
+def boucle_session():
+    global session_ouverte, derniere_carte_detectee
+    if not session_ouverte:
+        # Essai de connexion
+        robot.connecter()
+        if robot.verifier_session():
+            print(f"Hello \033[96;1m{robot.utilisateur_connecte.first_name} {robot.utilisateur_connecte.last_name}\033[0;0m !")
+        else:
+            # Affichage de carte détéctée non connectée
+            carte_detectee = robot.detecter_carte()
+            if carte_detectee:
+                robot.afficher_carte_detectee(carte_detectee, 980, 200)
+                derniere_carte_detectee = carte_detectee
+
+
+# --- BOUTONS ---
+# - Preparation -
+bouton_deconnexion = robot.creer_bouton(200, 60, 25, 200, Couleur.ORANGE)
+bouton_deconnexion.ajouter_texte("Deconnexion", 5, 20)
+bouton_suppression = robot.creer_bouton(200, 60, 25, 300, Couleur.ROUGE)
+bouton_suppression.ajouter_texte("Supprimer utilisateur", 5, 20)
+bouton_creation = robot.creer_bouton(200, 60, 980, 100, (0, 150, 0))
+bouton_creation.ajouter_texte("Créer  utilisateur", 20, 20)
+
+# - Boucle -
+def boucle_boutons():
     global mettre_a_jour_affichage, session_ouverte, carte_detectee
     # Mise à jour du status de la session
     if robot.verifier_session() != session_ouverte:
@@ -63,28 +97,11 @@ def verifier_boutons():
         session_ouverte = False
         mettre_a_jour_affichage = True
 
-def boucle_programme():
-    global mettre_a_jour_affichage, session_ouverte, derniere_carte_detectee
-    while robot.est_actif():
-        events = robot.verifier_evenements()
-        if "stop" in events:
-            robot.fermer_fenetre()
-        robot.afficher_camera(300, 10)
-        if not session_ouverte:
-            # Essai de connexion
-            robot.connecter()
-            if robot.verifier_session():
-                print(f"Hello \033[96;1m{robot.utilisateur_connecte.first_name} {robot.utilisateur_connecte.last_name}\033[0;0m !")
-            else:
-                # Affichage de carte détéctée non connectée
-                carte_detectee = robot.detecter_carte()
-                if carte_detectee:
-                    robot.afficher_carte_detectee(carte_detectee, 980, 200)
-                    derniere_carte_detectee = carte_detectee
-        affichage_ecran()
-        verifier_boutons()
-        robot.actualiser_affichage()
-
 
 if __name__ == "__main__":
-    boucle_programme()
+    while robot.est_actif():
+        boucle_evenements()
+        boucle_fenetre()
+        boucle_session()
+        boucle_boutons()
+        robot.actualiser_affichage()
