15d840924d19cb978de977035e05943a3a080da5
Thibo_M <thibaud.marin@proton.me>
2024-01-26 02:04:16 +0100

    Default main udpated
    Structured in different files, and contains now:
      - Session management by card detection
      - IA input box and button to switch

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from v2_main_chapter5_boucle import boucle_boutons, boucle_fenetre, boucle_session, boucle_evenements
-from v2_main_chapter5_initialiation import robot, initialisation_boutons, initialisation_evenements, initialisation_fenetre, initialisation_session
+from main_boucle import boucle_boutons, boucle_fenetre, boucle_session, boucle_evenements
+from main_initialisation import robot, initialisation_boutons, initialisation_evenements, initialisation_fenetre, initialisation_session
 
 
 if __name__ == "__main__":
-    initialisation_boutons()
-    initialisation_evenements()
     initialisation_fenetre()
+    initialisation_evenements()
+    initialisation_boutons()
     initialisation_session()
     while robot.est_actif():
         boucle_evenements()
+        boucle_boutons()
         boucle_fenetre()
         boucle_session()
-        boucle_boutons()
         robot.actualiser_affichage()
