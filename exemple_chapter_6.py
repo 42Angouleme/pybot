4258cb2ae4059d31477d37c1b0ba1dcd924dcef5
eliot <eliotblnd42@gmail.com>
2024-01-29 10:34:57 +0100

    Merge Done

--- exemple_chapter_6.py
+++ exemple_chapter_6.py
@@ -49,12 +49,11 @@ bouton_question.ajouter_texte("Poser question", 5, 20)
 bouton_stop = robot.creer_bouton(200, 60, 10, 300, Couleur.ROUGE)
 bouton_stop.ajouter_texte("Quitter", 10, 10, 20)
 text_area = robot.creer_zone_texte(400, 100, 600, 200, Couleur.GRIS)
-text_area_bis = robot.creer_zone_texte(400, 100, 600, 350, Couleur.GRIS)
 text_area.modifier_couleur_police(Couleur.VERT_SAPIN)
 
 # - Boucle -
 def boucle_boutons():
-    global mettre_a_jour_affichage, discussion_commencer, text_area, text_area_bis
+    global mettre_a_jour_affichage, discussion_commencer, text_area
     if bouton_question.est_actif():
         discussion_commencer = not discussion_commencer
         if discussion_commencer:
@@ -69,10 +68,6 @@ def boucle_boutons():
     if discussion_commencer and text_area.est_actif():
         texte_utilisateur = robot.ecrire(text_area)
         robot.repondre_question(texte_utilisateur)
-    if discussion_commencer and text_area_bis.est_actif():
-        texte_utilisateur = robot.ecrire(text_area_bis)
-        text_area_bis.effacer_texte()
-        # robot.repondre_question(texte_utilisateur)
 
 
 if __name__ == "__main__":
