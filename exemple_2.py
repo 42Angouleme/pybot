dd4ec5edc9d700a7c0f4529a3cf743d4fe6a3dd2
eliot <eliotblnd42@gmail.com>
2024-03-19 16:17:39 +0100

    New exemple

---
+++ exemple_2.py
@@ -0,0 +1,44 @@
+from pybot import Robot
+
+
+# ---- Dans main_initialisation.py -------
+robot = Robot()
+
+long = 1024
+haut = 800
+
+robot.initialiser_module_fenetre()
+
+robot.fenetre.ouvrir_fenetre(long, haut)
+robot.fenetre.changer_titre("Bonjour!")
+
+def initialisation_evenements():
+    print("Creation des eveneemnts sur les touches <ECHAP> ou <C>")
+    robot.ajouter_evenement("echap", "stop")
+    robot.ajouter_evenement("C", "carotte")
+# ----------------------------------------
+
+# -------- Dans main_boucle.py -----------
+def boucle_evenements():
+    evenements = robot.verifier_evenements()
+    if "stop" in evenements:
+        robot.desactiver()
+    elif "carotte" in evenements:
+        print("Suppression de l' évenement de la touche <C>")
+        robot.supprimer_evenement("carotte")
+        print("Création de l' évenement de la touche <P>")
+        robot.ajouter_evenement("P", "poireau")
+    elif "poireau" in evenements:
+        print("Suppression des évenements de la touche <P> et <Echap>")
+        robot.supprimer_evenement("poireau")
+        robot.supprimer_evenement("stop")
+        print("Pour quitter tu dois maintenant utiliser: <Q>")
+        robot.ajouter_evenement("Q", "stop")
+# ----------------------------------------
+
+
+if __name__ == '__main__':
+    initialisation_evenements()
+    while robot.est_actif():
+        boucle_evenements()
+        robot.fenetre.actualiser_affichage()
