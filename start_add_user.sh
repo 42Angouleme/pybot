182e5a48d79aeb351f0ef273004b72f29d8e9e86
eliot <eliotblnd42@gmail.com>
2024-05-07 15:51:01 +0200

    Improve script

--- start_add_user.sh
+++ start_add_user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/eliot/Documents/"Projet Robot"/pybot
+cd /home/pybot/pybot
 python3 -m venv --system-site-packages venv
 source venv/bin/activate
 python3 -m pip install -Ir requirements.txt
