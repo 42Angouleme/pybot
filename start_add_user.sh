7b7456817f9c7fa7fbd0eed7538ae1782d55dc83
eliot <eliotblnd42@gmail.com>
2024-05-07 15:44:25 +0200

    Start Porject Raspberry version

--- start_add_user.sh
+++ start_add_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/eliot/Documents/"Projet Robot"/pybot
-python3 -m venv venv
+python3 -m venv --system-site-packages venv
 source venv/bin/activate
-python3 -m pip install -r requirements.txt
+python3 -m pip install -Ir requirements.txt
 python3 add_user_to_database.py
