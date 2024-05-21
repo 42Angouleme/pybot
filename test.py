0f6ccb0f9f2db03a55177bc9d7506d62e9a7445a
eliot <eliotblnd42@gmail.com>
2024-05-21 14:04:13 +0200

    Test

--- test.py
+++ test.py
@@ -1,48 +1,22 @@
 import pygame
+from pygame.locals import *
 
-pygame.init()
-window = pygame.display.set_mode((300, 300))
-clock = pygame.time.Clock()
+def main():
 
-buttons = [
-    pygame.Rect(25, 25, 100, 100),
-    pygame.Rect(175, 25, 100, 100),
-    pygame.Rect(25, 175, 100, 100),
-    pygame.Rect(175, 175, 100, 100)]
-colors = [(64, 0, 0), (64, 64, 0), (0, 64, 0), (0, 0, 64)]
-colorsH = [(255, 0, 0), (255, 255, 0), (0, 255, 0), (0, 0, 255)]
+        pygame.init()
+        pygame.mouse.set_visible(0)
+        screenWidth  = pygame.display.Info().current_w
+        screenHeight = pygame.display.Info().current_h
+        #print(screenWidth)
+        #print(screenHeight)
+        size = (screenWidth, screenHeight)
+        screen = pygame.display.set_mode(size)
 
-fingers = {}
-run = True
-while run:
-    clock.tick(60)
+        while True:
+                for event in pygame.event.get():
+                        if(event.type is pygame.FINGERDOWN):
+                                x = event.x
+                                y = event.y
+                                print(x,y)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.FINGERDOWN or event.type == pygame.FINGERMOTION:
-            x = event.x * window.get_height()
-            y = event.y * window.get_width()
-            fingers[event.finger_id] = x, y
-        if event.type == pygame.FINGERUP:
-            fingers.pop(event.finger_id, None)
-
-    highlight = []  
-    for i, rect in enumerate(buttons): 
-        touched = False
-        for finger, pos in fingers.items():       
-            if rect.collidepoint(pos):
-                touched = True
-        highlight.append(touched)   
-
-    # the same with list comprehensions
-    #highlight = [any(r.collidepoint(p) for _, p in fingers.items()) for _, r in enumerate(buttons)]
-
-    window.fill(0)
-    for rect, color, colorH, h in zip(buttons, colors, colorsH, highlight):
-        c = colorH if h else color
-        pygame.draw.rect(window, c, rect)
-    pygame.display.flip()
-
-pygame.quit()
-exit()
+main()
